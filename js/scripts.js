d14c0cc4c4a9dae86dce987eb9b75c173b96eea3
amanyKassem <amany.kassem95@gmail.com>
2020-12-10 16:45:24 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+
+
+    //scroll top
+    var scrollButton = $("#scroll-top");
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 700) {
+            scrollButton.fadeIn(1000);
+        } else {
+            scrollButton.fadeOut(1000);
+        }
+    });
+
+    //click to scroll top
+    scrollButton.click(function () {
+        $('html,body').animate({scrollTop: 0}, 600);
+    });
+
+});
+
+/* loading screen */
+$(window).on('load', function () {
+
+    $(".layer-preloader").fadeOut(700, function () {
+
+        $(".lds-dual-ring").delay(1000).fadeOut(700);
+
+        $("body").css("overflow-y", "auto");
+
+    });
+
+});
