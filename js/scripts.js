9c79311c14768ba4314441757d9f1d3df0d8bb64
Amany Kassem <amanykassem@Amany.local>
2020-12-16 16:51:06 +0200

    login , index, notifications & comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,39 @@
 $(document).ready(function () {
 
+    // toggle menu
+    $("header .toggle").click(function () {
+        $(".overlay").css({
+            "transform": "scaleX(1)"
+        });
 
+        $(".menu").addClass('ulDir');
+
+    });
+
+    $("header .overlay").click(function () {
+        $(this).removeAttr("style");
+        $(".menu").removeClass("ulDir");
+    });
+
+    // toggle dashboard menu
+
+    $("header .toggleSideMenu").click(function () {
+        $(".dashContent .sideMenu").toggleClass('ulDir');
+        $(".content").toggleClass('w-78 w-100');
+
+    });
+
+    // side menu li active
+    // $(".mainUl li a").click(function () {
+    //     $(this).parent().addClass("active").siblings().removeClass("active");
+    // });
+
+
+    // sub menu
+    // $('.sub-menu ul').hide();
+    // $(".sub-menu a").click(function () {
+    //     $(this).parent(".sub-menu").children("ul").slideToggle("100");
+    // });
 
     //scroll top
     var scrollButton = $("#scroll-top");
