f70e73224969a03731b112295a846382ac66ab95
Amany Kassem <amanykassem@Amany.local>
2020-12-21 16:48:52 +0200

    orders, incoming orders , order details & offers

--- js/scripts.js
+++ js/scripts.js
@@ -45,6 +45,30 @@ $(document).ready(function () {
     });
 
 
+    // ADD IMAGE
+    $('.imageUploader').change(function (event){
+        $(this).parents('.imagesUploadBlock').append('<div class="uploadedBlock"><img src="'+ URL.createObjectURL(event.target.files[0]) +'"><button class="close"><i class="fas fa-times"></i></button></div>');
+    });
+
+    // REMOVE IMAGE
+    $('.dropBox').on('click', '.close',function (){
+        $(this).parents('.textCenter').remove();
+    });
+
+    $(".clickAdd").click(function (b){
+        b.preventDefault();
+        $('.dropBox').append('<div class="textCenter">' + '<div class="imagesUploadBlock">' + '<label class="uploadImg">' + '<span><i class="far fa-image"></i></span>' + '<input type="file" accept="image/*" class="imageUploader">' + '</label>' + '</div>' + '</div>');
+
+        $('.imageUploader').change(function (event){
+            $(this).parents('.imagesUploadBlock').append('<div class="uploadedBlock"><img src="'+ URL.createObjectURL(event.target.files[0]) +'"><button class="close"><i class="fas fa-times"></i></button></div>');
+        });
+        $('.dropBox').on('click', '.close',function (){
+            $(this).parents('.textCenter').remove();
+        });
+
+    });
+
+
     //scroll top
     var scrollButton = $("#scroll-top");
     $(window).scroll(function () {
